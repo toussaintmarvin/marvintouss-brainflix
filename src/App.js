fe0c558fdc3d61e4d0519ddef6dc61f035901559
toussaintmarvin <marvin.toussaint@gmail.com>
2024-01-28 21:16:21 -0500

    Adding sprint 1 updates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+//import state
+import { useState } from 'react';
+
+//import data
+import Video from './data/video-details.json'
+
+//import components
+import Header from './components/Header/Header'
+import Hero from './components/Hero/Hero'
+import CommentSection from './components/CommentSection/CommentSection';
+import VideoList from './components/VideoList/VideoList';
+import HeroContent from './components/HeroContent/HeroContent';
+
+//import styling
+import './App.scss';
 
 function App() {
+
+  //create a state hook
+  const [currentVideoId, setVideoId] = useState(Video[0].id);
+  // find the details of selected video from the data array
+  // id is being set in VideoList component 
+  let selectedVideo = Video.find(e => e.id === currentVideoId)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+    <Header /> 
+    <Hero 
+      // pass selected video's detailed data
+      //Hero component displays the selected video
+      selectedVideo = {selectedVideo}
+    />
+    {/* container for desktop breakpoint layout purposes*/}
+    <section className='container'>
+      
+      <section className='container__left'>
+        <HeroContent 
+          // HeroContent displays the selected video's content
+          //pass selected video's detailed data
+          selectedVideo = {selectedVideo}
+        />
+        <CommentSection 
+          //pass selected video's detailed data
+          selectedVideo = {selectedVideo}
+        />
+      </section>
+
+      <VideoList
+        setVideoId = {setVideoId}
+        //pass only selected video's Id 
+        //since we already have the required data in this component
+        selectedVideoId = {selectedVideo.id}
+      />
+
+    </section>
+  </>  
   );
 }
 
