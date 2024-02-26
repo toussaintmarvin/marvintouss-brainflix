5d38c941033aac6e1f95243c9b2b698a29978853
toussaintmarvin <marvin.toussaint@gmail.com>
2024-02-25 19:18:45 -0500

    hero video break fix

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -7,6 +7,7 @@ const Hero = ({selectedVideo}) => {
         return <h2>Video Not Found</h2>
     }
     return (
+        
         <div className='hero__video-container'>
             <video controls className='hero__video' poster={selectedVideo.image}>
             <source src={`${selectedVideo.video}?api_key=9bd6ead6-03ac-4347-852d-ca77aaf28bd9`} type=""/>
